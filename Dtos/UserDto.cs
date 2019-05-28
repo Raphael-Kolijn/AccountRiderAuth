31ba78f839c9a73d43cb3fa7f1f7e4d9bd1f3ab8
Raphael Kolijn <raphaelkolijn@gmail.com>
2019-05-28 16:41:47 +0200

    initial commit

---
+++ Dtos/UserDto.cs
@@ -0,0 +1,10 @@
+namespace WebApi.Dtos
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string BSN { get; set; }
+        public string Password { get; set; }
+    }
+}
